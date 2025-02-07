d598f82f6afbbb63244cb437797157954e76a731
Samuel Redd <samredd@MacBook-Air-29.local>
2025-02-07 15:11:24 -0700

    making some more .jsx files

--- src/allTimeHighScore/allTimeHighScore.jsx
+++ src/allTimeHighScore/allTimeHighScore.jsx
@@ -1,9 +1,35 @@
 import React from "react";
+import "./form.css";
 
 export function AllTimeHighScore() {
   return (
-    <main className="container-fluid bg-secondary text-center">
-      <div>allTimeHighScore displayed here</div>
+    <main>
+      <table>
+        <thead>
+          <tr>
+            <th>Rank</th>
+            <th>Player</th>
+            <th>Score</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>1</td>
+            <td>Player1</td>
+            <td>1000</td>
+          </tr>
+          <tr>
+            <td>2</td>
+            <td>Player2</td>
+            <td>900</td>
+          </tr>
+          <tr>
+            <td>3</td>
+            <td>Player3</td>
+            <td>800</td>
+          </tr>
+        </tbody>
+      </table>
     </main>
   );
 }
--- src/gameplay/gameplay.jsx
+++ src/gameplay/gameplay.jsx
@@ -1,9 +1,38 @@
 import React from "react";
+import "./gampley.css";
 
 export function Gameplay() {
   return (
-    <main className="container-fluid bg-secondary text-center">
-      <div>gameplay displayed here</div>
+    <main>
+      <p>This is where the game will be played</p>
+      <p>
+        When you hit incrament of 50 points it will communicate over websocket
+        to everyone on the server/webpage that you have hit that point
+      </p>
+      <p>It will appear in the top right corner</p>
+      <p>Your score and username will saved in the database</p>
+      <p>(The image has the websocket and the currect score reversed)</p>
+
+      {/* <a href="gameplayPage.png">
+        <img
+          src="gameplayPage.png"
+          alt="gameplayPage"
+          style="width: 750px; height: 750px"
+        />
+      </a>
+
+      <p>
+        When you get hit it will call the public API and show this explosion
+        emoji
+      </p>
+
+      <a href="explosion.jpg">
+        <img
+          src="explosion.jpg"
+          alt="explosion"
+          style="width: 500px; height: 500px"
+        />
+      </a> */}
     </main>
   );
 }
